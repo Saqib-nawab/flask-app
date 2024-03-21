3ed257a90af35aa727b16d6bd9f02e3562216120
Saqib <saqibnawab823@gmail.com>
2024-03-21 15:47:36 +0500

    database files restructured

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///instance/todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.app_context().push()
 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
